bea3b0bdfc56c8e91e19ed0a59367bcca362fee2
tonygj <tonygj@stud.ntnu.no>
2014-03-03 11:36:14 +0100

    Her er kalenderklient

---
+++ ProjectPU/src/Classes/CalendarClient.java
@@ -0,0 +1,17 @@
+package Classes;
+
+public class CalendarClient {
+
+	public static void main(String[] args) {
+		
+	}
+
+	public String showWeekCalendar(){
+		
+	}
+	
+	public String showMyCalendar(){
+		
+		
+	}
+}
