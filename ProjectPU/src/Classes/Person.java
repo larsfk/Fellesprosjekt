150696d3dea79189bf1bb2c67b94fbd03a0c28b3
tonygj <tonygj@stud.ntnu.no>
2014-03-04 10:10:55 +0100

    hei

--- ProjectPU/src/Classes/Person.java
+++ ProjectPU/src/Classes/Person.java
@@ -7,8 +7,12 @@ public class Person {
 	Calendar calendar; //La den til siden jeg trenger denne i calendarClient
 	String email; //Denne ogsaa
 	
-	public static Person(){
-		
+	public Person(String name, String adress, int SSN, Calendar calendar, String email){
+		this.name = name;
+		this.adress = adress;
+		this.SSN = SSN;
+		this.calendar = calendar;
+		this.email = email;
 	}
 
 }
