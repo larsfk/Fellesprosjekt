35fefdd693ff8a049258a3eba83cd40957ee2e58
tonygj <tonygj@stud.ntnu.no>
2014-03-04 10:42:40 +0100

    ny update

--- ProjectPU/src/Classes/Person.java
+++ ProjectPU/src/Classes/Person.java
@@ -3,12 +3,11 @@ package Classes;
 import java.sql.Date;
 
 public class Person {
-	String name;
-	String adress;
-	int SSN;
-	Calendar calendar; //La den til siden jeg trenger denne i calendarClient
-	String email; //Denne ogsaa
-//	Appointment appointment = new Appointment(appID, stime, ftime, dur, meetpl, descr, alarm);
+	private String name;
+	private String adress;
+	private int SSN; 
+	private String email; 
+	Calendar calendar = new Calendar();
 	
 	public Person(String name, String adress, int SSN, Calendar calendar, String email){
 		this.name = name;
@@ -17,8 +16,37 @@ public class Person {
 		this.calendar = calendar;
 		this.email = email;
 	}
-
-	public void setAppointment(int appID, Date stime, Date ftime, ){
-		
+	
+	public String getName() {
+		return name;
 	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAdress() {
+		return adress;
+	}
+
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+
+	public int getSSN() {
+		return SSN;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setAppointment(Appointment appoint){
+		calendar.addAppointment(appoint);
+	}
+	
 }
