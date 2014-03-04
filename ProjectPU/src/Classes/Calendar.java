fada8e832cf78067f76117c5e47c59f1b66ab1a5
idasag <idasag@stud.ntnu.no>
2014-03-04 11:33:09 +0100

    lagt til getAppointments

--- ProjectPU/src/Classes/Calendar.java
+++ ProjectPU/src/Classes/Calendar.java
@@ -11,7 +11,9 @@ public class Calendar {
 	public String getUserEmail(){
 		return userEmail;
 	}
-	
+	public ArrayList<Appointment> getAppointments(){
+		return appointments;
+	}
 	public void hideAppointment(Appointment appoint){
 		if (appointments.contains(appoint)){
 			appoint.setHidden(true);			
@@ -31,23 +33,23 @@ public class Calendar {
 	}
 	
 	public void editStarttime(Appointment appoint, Date starttime){
-		appoint.starttime = starttime;
+		appoint.setStarttime(starttime);
 	}
 	
 	public void editFinishtime(Appointment appoint, Date finishtime){
-		appoint.finishingtime = finishtime;
+		appoint.setFinishingtime(finishtime);
 	}
 	
 	public void editDuration(Appointment appoint, int duration){
-		appoint.duration = duration;
+		appoint.setDuration(duration);
 	}
 	
 	public void editMeetingplace(Appointment appoint, String meetpl){
-		appoint.meetingplace = meetpl;
+		appoint.setMeetingplace(meetpl);
 	}
 	
 	public void editDescription(Appointment appoint, String descr){
-		appoint.description = descr;
+		appoint.setDescription(descr);
 	}
 	
 	public void editAlarm(Appointment appoint, Date alarm){
