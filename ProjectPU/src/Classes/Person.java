4febe320d93fb474df99bb01a9a0f75c5c9fcd4d
tonygj <tonygj@stud.ntnu.no>
2014-03-04 09:55:32 +0100

    test

--- ProjectPU/src/Classes/Person.java
+++ ProjectPU/src/Classes/Person.java
@@ -3,11 +3,6 @@ package Classes;
 public class Person {
 	String name;
 	String adress;
-
-	int tall;
-	Calendar calendar; //La den til siden jeg trenger denne i calendarClient
-	String email; //Denne ogsaa
-
 	int SSN;
 	Calendar calendar; //La den til siden jeg trenger denne i calendarClient
 	String email; //Denne ogsaa
