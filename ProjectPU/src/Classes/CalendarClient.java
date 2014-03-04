3f160ce212d3649bb4ab2ae03971a97652bd358c
tonygj <tonygj@stud.ntnu.no>
2014-03-04 09:50:20 +0100

    hei

--- ProjectPU/src/Classes/CalendarClient.java
+++ ProjectPU/src/Classes/CalendarClient.java
@@ -4,25 +4,29 @@ public class CalendarClient {
 
 	Calendar calendar = new Calendar();
 	Person person = new Person();
-	
-//	public static void main(String[] args) {
-//		
-//	}
-	
+
+	//	public static void main(String[] args) {
+	//		
+	//	}
+
 	//Logge inn skal ligge i calendarClient - ulikt klassediagram
 	public boolean loggedIn(){
-		if 
-	}
-
-	public String showWeekCalendar(){
-		
-		return null;
+		String email = person.email;
+		String password;
+//		if()
 	}
 	
+	
+
+	public String showWeekCalendar(){
+
+		return null;
+	}
+
 	public String showMyCalendar(){
 		return null;
 	}
-	
+
 	public String showGroupCalendar(){
 		return null;
 	}
--- ProjectPU/src/Classes/Person.java
+++ ProjectPU/src/Classes/Person.java
@@ -3,7 +3,11 @@ package Classes;
 public class Person {
 	String name;
 	String adress;
-	int tall;
+	int SSN;
 	Calendar calendar; //La den til siden jeg trenger denne i calendarClient
 	String email; //Denne også
+	
+	public static Person(){
+		
+	}
 }
