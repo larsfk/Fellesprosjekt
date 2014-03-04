2644282a0e2c03f314dbd06b65c82f53cc19472f
tonygj <tonygj@stud.ntnu.no>
2014-03-04 11:51:45 +0100

    Oppdatert calendarClient

--- ProjectPU/src/Classes/CalendarClient.java
+++ ProjectPU/src/Classes/CalendarClient.java
@@ -1,30 +1,31 @@
 package Classes;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class CalendarClient {
 
 	Calendar calendar = new Calendar();
 	Person person;
 	private ArrayList<Calendar> ar = new ArrayList<Calendar>();
+	public Date date = new Date();
+	public int today = date.getDate();
+	public int day = date.getDay();
+	public int month = date.getMonth();
 
 	//Lage database med hvilken kalender som tilhører hvem!
-	
-	//	public static void main(String[] args) {
-	//		
-	//	}
+
+//	public static void main(String[] args) {
+//		System.out.println();
+//	}
 
 
 	public ArrayList<Calendar> getAr(){
 		return ar;
 	}
-	
-	public String showWeekCalendar(){
-		
-	}
 
-	public String showMyCalendar(){
-		return null;
+	public String showMyWeekCalendar(){ //Slått sammen denne med showMyCalendar
+		
 	}
 
 	public String showGroupCalendar(){
---
+++ ProjectPU/src/Classes/DateTest.java
@@ -0,0 +1,15 @@
+package Classes;
+
+import java.util.Date;
+
+public class DateTest {
+	
+	public static Date date = new Date();
+
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) {
+		System.out.println("" + date.getDate());
+
+	}
+
+}
