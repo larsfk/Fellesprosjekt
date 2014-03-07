1e51637c4797383ef583a808bedd4531eda54d7d
oystein924 <oystein924>
2014-03-07 15:44:16 +0100

    FIKK LASTET PER OPP I DATABASEN! #YOLO

--- ProjectPU/src/Classes/Database.java
+++ ProjectPU/src/Classes/Database.java
@@ -6,12 +6,36 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 
 public class Database {
-	static Connection connection = null;
-	static String input = "insert into larsfkl_felles.person(name,office,tlf,email,password) values ('Test','NTNU',94234135,'test@test.no','mojjo')";
 	
-	public static void main(String[] args) throws Exception{
+//	String userName;
+//	String password;
+//	//static String input = "insert into larsfkl_felles.person(name,office,tlf,email,password) values ('Test','NTNU',94234135,'test@test.no','mojjo')";
+
+	//	public static void main(String[] args) throws Exception{
+
+	public Connection getConnection() throws SQLException {
+
+	    Connection conn = null;
+//	    Properties connectionProps = new Properties();
+//	    connectionProps.put("larsfkl_felles", this.userName);
+//	    connectionProps.put("bademadrass", this.password);
+//	    connectionProps.setProperty("userName", "larsfkl_felles");
+//	    connectionProps.setProperty("password", "bademadrass");
+	    conn = DriverManager.getConnection("jdbc:mysql://mysql.stud.ntnu.no:3306/larsfkl_felles","larsfkl_felles","bademadrass");
+//	    conn = DriverManager.getConnection("jdbc:mysql://mysql.stud.ntnu.no:3306/larsfkl_felles",  connectionProps);
+	    System.out.println("Connected to database");
+	    return conn;
+	}
+	
+
+	public Connection getConnectionLars(){
+		Connection conn = null;
+//		connectionProps.put("user", this.userName);
+//	    connectionProps.put("password", this.password);
+		
 		
 		try{
 			//Accessing driver from the JAR file needed to use mySQL
@@ -19,40 +43,47 @@ public class Database {
 		} catch (ClassNotFoundException e){
 			System.out.println("Where is MySQL JDBC Driver?");
 			e.printStackTrace();
-			return;
+			return null;
 		}
-		
+
 		try{
 			//Creating a variable for the connection called "con"
-			connection = DriverManager.getConnection("jdbc:mysql://mysql.stud.ntnu.no:3306/larsfkl_felles","larsfkl_felles","bademadrass");
+			conn = DriverManager.getConnection("jdbc:mysql://mysql.stud.ntnu.no:3306/larsfkl_felles","larsfkl_felles","bademadrass");
+			if (conn != null){
+				System.out.println("Klarte å koble til :D");
+				return conn;
+			}
 			//Username: larsfkl_felles
 			//PW: bademadrass
 		} catch (SQLException e){
 			System.out.println("Connection failed...");
 			e.printStackTrace();
-			return;
+			return null;
 		}
-		
-		if(connection == null){
+
+		if(conn == null){
 			System.out.println("Failed to connect...");
+			return null;
 		}
-		addToDatabase(input);
+		//addToDatabase(input);
+		return null;
 	}
-	
-	public static void addToDatabase(String statement) throws SQLException{
-			//Create a query
-			Statement stmt = (Statement) connection.createStatement();
-			//Execute query
-			stmt.executeUpdate(statement);
+	//	}
+
+	public void addToDatabase(String statement, Connection conn) throws SQLException{
+		//Create a query
+		Statement stmt = (Statement) conn.createStatement();
+		//Execute query
+		stmt.executeUpdate(statement);
 
 	}
-	
-	public static void readDatabase(String res) throws SQLException{
+
+	public static void readDatabase(String res, Connection conn) throws SQLException{
 		//Create a query
-		PreparedStatement query = connection.prepareStatement(res);
+		PreparedStatement query = conn.prepareStatement(res);
 		//Creating a variable to execute query
 		ResultSet result = query.executeQuery();
-			
+
 		while(result.next()){
 			System.out.println("Name: " + result.getString(1));
 
