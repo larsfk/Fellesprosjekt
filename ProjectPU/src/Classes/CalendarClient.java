46345dbe5ad373f8c5679217d76d640c16ffd176
tonygj <tonygj@stud.ntnu.no>
2014-03-07 15:25:34 +0100

    Ny CalendarClient

--- ProjectPU/src/Classes/CalendarClient.java
+++ ProjectPU/src/Classes/CalendarClient.java
@@ -12,6 +12,7 @@ public class CalendarClient {
 	private ArrayList<PUCalendar> calendarList = new ArrayList<PUCalendar>();
 	private ArrayList<ArrayList<Appointment>> myCalendar = new ArrayList<ArrayList<Appointment>>();
 	public Calendar calendar = Calendar.getInstance();
+	ArrayList<Appointment> appointments = new ArrayList<Appointment>();
 
 	//Lage database med hvilken kalender som tilhører hvem!
 
@@ -27,32 +28,36 @@ public class CalendarClient {
 		calendarList.add(calendar);
 	}
 
-	public static void main(String[] args){
-CalendarClient cc = new CalendarClient();
-		
-		PUCalendar cal1 = new PUCalendar("rebcox@gmail.com");
-		PUCalendar cal2 = new PUCalendar("tonygj@gmail.com");
-		PUCalendar cal3 = new PUCalendar("ida@sagdahl.com");
-		Calendar date1 = Calendar.getInstance();
-		Calendar date2 = Calendar.getInstance();
-		Calendar date3 = Calendar.getInstance();
-		Date thisDate = new Date();
-		
-		Appointment appoint1; 
-		
-		date1.set(thisDate.getYear(), thisDate.getMonth(), thisDate.getDate(), 14, 00);
-		date2.set(thisDate.getYear(), thisDate.getMonth(), thisDate.getDate(), 16, 00);
-		appoint1 = new Appointment(1,date1,date2,"Hos Cox","Progging",date3);
-		
-		cal1.addAppointment(appoint1);
-		
-		
-		cc.addCalendar(cal1);
-		cc.addCalendar(cal2);
-		cc.addCalendar(cal3);
-		
-		System.out.println(cal1.getAppointments());
-	}
+	//	public static void main(String[] args){
+	//		CalendarClient cc = new CalendarClient();
+	//		CalendarClient cc1 = new CalendarClient();
+	//		
+	//		PUCalendar cal1 = new PUCalendar("rebcox@gmail.com");
+	//		PUCalendar cal2 = new PUCalendar("tonygj@gmail.com");
+	//		PUCalendar cal3 = new PUCalendar("ida@sagdahl.com");
+	//		Calendar date1 = Calendar.getInstance();
+	//		Calendar date2 = Calendar.getInstance();
+	//		Calendar date3 = Calendar.getInstance();
+	//		Date thisDate = new Date();
+	//		
+	//		Appointment appoint1; 
+	//		Appointment appoint2; 
+	//		
+	//		date1.set(thisDate.getYear(), thisDate.getMonth(), thisDate.getDate(), 14, 00);
+	//		date2.set(thisDate.getYear(), thisDate.getMonth(), thisDate.getDate(), 16, 00);
+	//		appoint1 = new Appointment(1,date1,date2,"Hos Cox","Progging",date3);
+	//		appoint2 = new Appointment(2,date1, date2, "Hos Tony", "progging ofc", date3);
+	//		
+	//		cal1.addAppointment(appoint1);
+	//		cal1.addAppointment(appoint2);
+	//		
+	//		
+	//		cc1.addCalendar(cal1);
+	//		cc.addCalendar(cal2);
+	//		cc.addCalendar(cal3);
+	//		
+	//		System.out.println(cc.showMyWeekCalendar());
+	//	}
 
 	public String showMyWeekCalendar(){ //Slått sammen denne med showMyCalendar
 		//		myCalendar.get(0).set(0, "Dag/tid");
@@ -64,11 +69,15 @@ CalendarClient cc = new CalendarClient();
 		//		myCalendar.get(6).set(0, "Lør");
 		//		myCalendar.get(7).set(0, "Søn");
 
-		if(person == null)
-			return "Du er ingen person!";
+		//		if(person.getPUCalendar() == null)
+		//			return "Du er ingen person!";
 
-		ArrayList<Appointment> appointments = person.getPUCalendar().getAppointments();
-		//		Collections.sort(appointments, new dateComparator());
+		//		ArrayList<Appointment> appointments = person.getPUCalendar().getAppointments();
+
+
+		for(int i = 0; i < calendarList.size(); i++){
+			appointments.addAll(calendarList.get(i).getAppointments());			
+		}
 
 
 		int date = calendar.DATE;
@@ -78,60 +87,86 @@ CalendarClient cc = new CalendarClient();
 		switch(dayOfWeek) {
 
 		case 0: {
+			int count = 0;
 			for(int i = 0; i< appointments.size(); i++){
-				if(appointments.get(i).getDate() == "" + calendar.DAY_OF_MONTH + "." + calendar.MONTH + "." + calendar.YEAR){
-					myCalendar.get(firstDayOfWeek + dayOfWeek).set(i, appointments.get(i));		
+				if(appointments.get(i).getDate() == "" + calendar.get(Calendar.DAY_OF_MONTH) + "." + calendar.get(Calendar.MONTH) + "." + calendar.get(Calendar.YEAR)){
+					myCalendar.get(firstDayOfWeek + dayOfWeek).set(count, appointments.get(i));		
+					count++;
 				}
 			}
 		}
 
 		case 1: {
-			for(int i = 0; i < appointments.size(); i++){
-				if(appointments.get(i).getDate() == "" + (calendar.DAY_OF_MONTH + dayOfWeek) + "." + calendar.MONTH + "." + calendar.YEAR){
-					myCalendar.get(firstDayOfWeek + dayOfWeek).set(i, appointments.get(i));		
+			int count = 0;
+			for(int i = 0; i< appointments.size(); i++){
+				if(appointments.get(i).getDate() == "" + (calendar.get(Calendar.DAY_OF_MONTH) + dayOfWeek) + "." + calendar.get(Calendar.MONTH) + "." + calendar.get(Calendar.YEAR)){
+					myCalendar.get(firstDayOfWeek + dayOfWeek).set(count, appointments.get(i));		
+					count++;
 				}
 			}
 		}
 
-		case 2:
-			for(int i = 0; i < appointments.size(); i++){
-				if(appointments.get(i).getDate() == "" + (calendar.DAY_OF_MONTH + dayOfWeek) + "." + calendar.MONTH + "." + calendar.YEAR){
-					myCalendar.get(firstDayOfWeek + dayOfWeek).set(i, appointments.get(i));		
+		case 2: {
+			int count = 0;
+			for(int i = 0; i< appointments.size(); i++){
+				if(appointments.get(i).getDate() == "" + (calendar.get(Calendar.DAY_OF_MONTH) + dayOfWeek) + "." + calendar.get(Calendar.MONTH) + "." + calendar.get(Calendar.YEAR)){
+					myCalendar.get(firstDayOfWeek + dayOfWeek).set(count, appointments.get(i));		
+					count++;
 				}
 			}
+		}
 
-		case 3:
-			for(int i = 0; i < appointments.size(); i++){
-				if(appointments.get(i).getDate() == "" + (calendar.DAY_OF_MONTH + dayOfWeek) + "." + calendar.MONTH + "." + calendar.YEAR){
-					myCalendar.get(firstDayOfWeek + dayOfWeek).set(i, appointments.get(i));		
+		case 3:{
+			int count = 0;
+			for(int i = 0; i< appointments.size(); i++){
+				if(appointments.get(i).getDate() == "" + (calendar.get(Calendar.DAY_OF_MONTH) + dayOfWeek) + "." + calendar.get(Calendar.MONTH) + "." + calendar.get(Calendar.YEAR)){
+					myCalendar.get(firstDayOfWeek + dayOfWeek).set(count, appointments.get(i));		
+					count++;
 				}
 			}
+		}
 
-		case 4:
-			for(int i = 0; i < appointments.size(); i++){
-				if(appointments.get(i).getDate() == "" + (calendar.DAY_OF_MONTH + dayOfWeek) + "." + calendar.MONTH + "." + calendar.YEAR){
-					myCalendar.get(firstDayOfWeek + dayOfWeek).set(i, appointments.get(i));		
+		case 4:{
+			int count = 0;
+			for(int i = 0; i< appointments.size(); i++){
+				if(appointments.get(i).getDate() == "" + (calendar.get(Calendar.DAY_OF_MONTH) + dayOfWeek) + "." + calendar.get(Calendar.MONTH) + "." + calendar.get(Calendar.YEAR)){
+					myCalendar.get(firstDayOfWeek + dayOfWeek).set(count, appointments.get(i));		
+					count++;
 				}
 			}
+		}
 
-		case 5:
-			for(int i = 0; i < appointments.size(); i++){
-				if(appointments.get(i).getDate() == "" + (calendar.DAY_OF_MONTH + dayOfWeek) + "." + calendar.MONTH + "." + calendar.YEAR){
-					myCalendar.get(firstDayOfWeek + dayOfWeek).set(i, appointments.get(i));		
+		case 5:{
+			int count = 0;
+			for(int i = 0; i< appointments.size(); i++){
+				if(appointments.get(i).getDate() == "" + (calendar.get(Calendar.DAY_OF_MONTH) + dayOfWeek) + "." + calendar.get(Calendar.MONTH) + "." + calendar.get(Calendar.YEAR)){
+					myCalendar.get(firstDayOfWeek + dayOfWeek).set(count, appointments.get(i));		
+					count++;
 				}
 			}
+		}
 
-		case 6:
-			for(int i = 0; i < appointments.size(); i++){
-				if(appointments.get(i).getDate() == "" + (calendar.DAY_OF_MONTH + dayOfWeek) + "." + calendar.MONTH + "." + calendar.YEAR){
-					myCalendar.get(firstDayOfWeek + dayOfWeek).set(i, appointments.get(i));		
+		case 6:{
+			int count = 0;
+			for(int i = 0; i< appointments.size(); i++){
+				if(appointments.get(i).getDate() == "" + (calendar.get(Calendar.DAY_OF_MONTH) + dayOfWeek) + "." + calendar.get(Calendar.MONTH) + "." + calendar.get(Calendar.YEAR)){
+					myCalendar.get(firstDayOfWeek + dayOfWeek).set(count, appointments.get(i));		
+					count++;
 				}
 			}
+		}
 		}
 		return myCalendar.toString();
 	}
 
 	public String showGroupCalendar(){
+
+		//		Må ha string i Participant med mail med tilhørende participants for å kunne vise kalenderne til alle i denne gruppen
+
+		//		ArrayList<Appointment> appointments = person.getPUCalendar().getAppointments();
+		//		for(int i = 0; i < appointments.size(); i++){
+		//			appointments.get(i).getParticipants();
+		//		}
 		return null;
 	}
 }
