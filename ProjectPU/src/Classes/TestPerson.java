a62506d37361349a703cf5cb615575c13ac4f106
idasag <idasag@stud.ntnu.no>
2014-03-10 13:28:41 +0100

    lagt til TestPerson

---
+++ ProjectPU/src/Classes/TestPerson.java
@@ -0,0 +1,15 @@
+package Classes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+	Person person = new Person("Tony", "P15", "81549300", "1234678901", "ilovemathilde");
+
+public class TestPerson {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
