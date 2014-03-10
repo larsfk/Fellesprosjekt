aad6f1229792194e87dbf14fed8da332a0e5630e
tonygj <tonygj@stud.ntnu.no>
2014-03-10 13:11:38 +0100

    Reminder-klasse

---
+++ ProjectPU/src/Classes/Reminder.java
@@ -0,0 +1,10 @@
+package Classes;
+
+public class Reminder extends Alarm {
+
+	public Reminder(int year, int month, int date, int hourOfDay, int minute,
+			String description) {
+		super(year, month, date, hourOfDay, minute, description);
+	}
+
+}
