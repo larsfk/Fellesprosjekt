6760439ac26c54f453c104088fddfb9d22ead649
larsfk <larsklingen@gmail.com>
2014-03-11 11:23:12 +0100

    Filen er tom

--- ProjectPU/src/Classes/TestPerson.java
+++ ProjectPU/src/Classes/TestPerson.java
@@ -2,14 +2,11 @@ package Classes;
 
 import static org.junit.Assert.*;
 
+import java.sql.Connection;
+
 import org.junit.Test;
-	Person person = new Person("Tony", "P15", "81549300", "1234678901", "ilovemathilde");
+
 
 public class TestPerson {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
 
 }
