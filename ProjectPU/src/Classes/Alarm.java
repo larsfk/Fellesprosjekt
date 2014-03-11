47063929729505d639e437c5ba626b81579a974c
idasag <idasag@stud.ntnu.no>
2014-03-11 15:14:35 +0100

    TestAlarm og Alarm endret

--- ProjectPU/src/Classes/Alarm.java
+++ ProjectPU/src/Classes/Alarm.java
@@ -8,11 +8,14 @@ public class Alarm {
 	private Calendar start;
 	
 	public Alarm(int year, int month, int date, int hourOfDay, int minute, String description){
-		this.start.set(year, month, date, hourOfDay, minute);
+		this.start = Calendar.getInstance();
+		this.setStart(year, month, date, hourOfDay, minute);
 		this.description = description;
+		
 	}
 	
 	public Alarm(Appointment app, int start, String description){
+		this.start = Calendar.getInstance();
 		this.start.add(app.getStarttime().MINUTE, -start);
 		this.description = description;
 	}
@@ -30,7 +33,17 @@ public class Alarm {
 	}
 
 	public void setStart(int year, int month, int date, int hourOfDay, int minute) {
-		this.start.set(year, month, date, hourOfDay, minute);
+		//this.start.set(year, month, date, hourOfDay, minute);
+		start.set(start.YEAR, year);
+		start.set(start.MONTH, month);
+		start.set(start.DAY_OF_MONTH, date);
+		start.set(start.HOUR_OF_DAY, hourOfDay);
+		start.set(start.MINUTE, minute);
+	}
+	
+	@Override
+	public String toString() {
+		return start.YEAR+"."+start.MONTH+"."+start.DAY_OF_MONTH+"."+start.HOUR_OF_DAY+"."+start.MINUTE;
 	}
 	
 }
---
+++ ProjectPU/src/Classes/TestAlarm.java
@@ -0,0 +1,48 @@
+package Classes;
+
+import static org.junit.Assert.*;
+
+import java.util.Calendar;
+
+import org.junit.Test;
+
+public class TestAlarm {
+
+	Alarm alarm1;
+	Calendar start1 = Calendar.getInstance();
+	Calendar finish1 = Calendar.getInstance();
+	Appointment app1;
+	Calendar date1 = Calendar.getInstance();
+	
+	public static void main(String[] args) {
+		TestAlarm testalarm = new TestAlarm();
+	}
+	
+	public TestAlarm(){
+		app1 = new Appointment(1, start1, finish1, "kontor 20", "mote", alarm1);
+		alarm1 = new Alarm(284, 3, 11, 14, 30, "Ha en god dag");
+		System.out.println(alarm1);
+		//date1.set(Calendar(2013, 3, 11, 14, 30));
+		alarm1.setStart(284, 3, 11, 14, 30);
+		date1.set(date1.YEAR, 2013);
+		date1.set(date1.MONTH, 3);
+		date1.set(date1.DAY_OF_MONTH, 11);
+		date1.set(date1.HOUR_OF_DAY, 14);
+		date1.set(date1.MINUTE, 30);
+		app1.setStarttime(date1);
+	}
+	
+	@Test
+	
+	public void testGetDate(){
+		assertEquals(date1, app1.getDate());
+	}
+	public void testGetDescription(){
+		assertEquals("Ha en god dag", alarm1.getDescription());
+	}
+	
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
