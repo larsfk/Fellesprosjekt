3cfad90a86b2bd52ff5197a344de9a3982dc2360
rebcox <rebecclc@stud.dtnu.no>
2014-03-12 11:48:08 +0100

    skrive inn passord, addAppointment + mer

--- ProjectPU/src/Classes/MainProgram.java
+++ ProjectPU/src/Classes/MainProgram.java
@@ -2,86 +2,108 @@ package Classes;
 
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.Scanner;
 
 public class MainProgram {
-	private String welcome = "Hello!\n"; //ID er index i kalenderlisten
-	
-	//Burde CalendarClient inneholde alle kalendere? I så fall skal denne listen fylles med disse:
-	
-	// CalendarClient cc; //= new CalendarClient();
-	
+	private String welcome = "Hello!\n";
+	private ArrayList<Person> validPersons;
+
 	private  void run() {
-		CalendarClient cc = new CalendarClient();		
+		//Kun testing fra her til.....
+		Person rebecca = new Person("Rebecca", "Home", "3423422", "rebcox@gmail.com","080674555","heisann");
+		CalendarClient cc = new CalendarClient(rebecca);		
 
 		Calendar date1 = Calendar.getInstance();
 		Calendar date2 = Calendar.getInstance();
-		Calendar date3 = Calendar.getInstance();
-		Date thisDate = new Date();
-		
-		date1.set(thisDate.getYear(), thisDate.getMonth(), thisDate.getDate(), 14, 00);
-		date2.set(thisDate.getYear(), thisDate.getMonth(), thisDate.getDate(), 16, 00);
-		
-		Person rebecca = new Person("Rebecca", "Home", "3423422", "rebcox@gmail.com","080674555","heisann");
-		
+		Alarm alarm1 = new Alarm(2013,03,18,12,30,"Kom!!");
+
+		date1.set(date1.HOUR_OF_DAY,10);
+		date2.set(date2.HOUR_OF_DAY, 11);
+
 		Appointment appoint1; 
-		appoint1 = new Appointment(1,date1,date2,"Hos Cox","Progging",date3,rebecca);		
-		
-		
+		appoint1 = new Appointment(date1,date2,"Hos Cox","Progging",alarm1,rebecca);
+		//....her
+
+		validPersons = new ArrayList<Person>();
+		/*
+		 * 
+		 * 
+		 * Fyll validPersons med alle tilgjengelige navn i databasen
+		 * 
+		 * 
+		 */
+
 		System.out.println(welcome);
 		Scanner sc = new Scanner(System.in);
-		//String input = sc.nextLine();
-		//while (input.compareTo("x")!=0){ //denne gjelder kun den ytteste løkka. problem om vi skal ha det med
-		
-//		int personID = -1; 
-//		while (personID < 0 || personID > calendars.size()){
-//			System.out.println("Who are you? Type number");
-//			for (int i = 0;i<calendars.size();i++){
-//				System.out.println("(" + i + ") " + calendars.get(i).getUserEmail()); //email is the identificator
-//			}
-//			personID = sc.nextInt();
-//		}
-		//Sammenligne passord med database
-		//activeCalendar = calendars.get(personID);
-		//cc.setPerson()
 
-		System.out.println("What would you like to do?\n1. Add appointment\n2. Delete appointment\n3. Show this calendar\n4. Show several calendars");
-		int option = sc.nextInt();
-		if (option==1){
-			
-			System.out.println("Type startTime");
-			String startTime = sc.nextLine(); //evt dele opp input til en liste
-			
-			//Appointment ap = new Appointment(startTime); //etc
-			//ELLER
-			//Add appointment(startTime) bør kanskje være boolean?
-
+		int personID = -1; 
+		while (personID < 0 || personID > validPersons.size()){ //Til vi faar et gyldig nr
+			System.out.println("Who are you? Type number");
+			for (int i = 0;i<validPersons.size();i++){
+				System.out.println("(" + i + ") " + validPersons.get(i).getName());
+			}
+			personID = sc.nextInt();
 		}
-		else if (option == 2){
-			int numApp = activeCalendar.getAppointments().size(); //Number of appointments in the active calendar
+		String password = ""; //Hente dette fra databasen
+		String typedPassword = ""; //kanskje "null"?
+		while (password.compareTo(typedPassword) != 0){
+			System.out.println("Type password");
+			typedPassword = sc.next();
+		}
+
+		System.out.println("What would you like to do?\n1. Add appointment\n2. Delete appointment\n3.Edit appointment\n4. Show this calendar\n5. Show several calendars");
+		int option = sc.nextInt();
+		
+		switch (option){
+		case 1:
+			System.out.println("Type starttime (yyyy.mm.dd.hh.mm)");
+			String time = sc.next();
+			//Burde sjekke om input er gyldig verdi om vi faar tid
+			String[] timeList = time.split("."); //splitter input paa punktum
+			Integer[] timeListInt = new Integer[5];
+			for (int i = 0;i<5;i++){
+				timeListInt[i] = Integer.parseInt(timeList[i]);
+			}
+			Calendar start = Calendar.getInstance();
+			start.clear();
+			start.set(timeListInt[0], timeListInt[1], timeListInt[2], timeListInt[3], timeListInt[4]);
+
+			System.out.println("Type finishime (yyyy.mm.dd.hh.mm)");
+			time = sc.next();
+			timeList = time.split("."); //splitter input paa punktum
+			for (int i = 0;i<5;i++){
+				timeListInt[i] = Integer.parseInt(timeList[i]);
+			}
+			Calendar finish = Calendar.getInstance();
+			finish.clear();
+			finish.set(timeListInt[0], timeListInt[1], timeListInt[2], timeListInt[3], timeListInt[4]);
+
+			//OSV
+
+			//Appointment ap = new Appointment(startTime, ); //etc
+
+		case 2:
+			int numApp = cc.getAppointmentList().size(); //Number of appointments in the active calendar
 			int appID = -1;
 			while (appID < 0){ // ||appID > numApp
 				System.out.println("Which appointment would you like to delete?");
 				for (int i = 0;i<numApp;i++){ 
-					System.out.println("(" + i + ") " + activeCalendar.getAppointments().get(i)); //returns element i in calendar appointments
+					System.out.println("(" + i + ") " + cc.getAppointmentList().get(i)); //returns element i in calendar appointments
 				}
 				appID = sc.nextInt();
 			}
-			activeCalendar.deleteAppointment(activeCalendar.getAppointments().get(appID));
-		}
-		else if (option == 3){
+			cc.deleteAppointment(cc.getAppointmentList().get(appID));
+		case 3:
+			//Edit appointment
+		case 4:
 			//Skrive ut kalender, TONY
-			//System.out.println(cc.showMyCalendar());
-		}
-		else if (option == 4){
+			//System.out.println(cc.showMyWeekCalendar());
+		case 5:
 			//Skrive ut flere kalendere, TONY
 			//System.out.println(cc.showGroupCalendar());
+		default:
+			System.out.println("What would you like to do?\n1. Add appointment\n2. Delete appointment\n3.Edit appointment\n4. Show this calendar\n5. Show several calendars");
 		}
-		else{
-			System.out.println("What would you like to do?\n1. Add appointment\n2. Delete appointment\n3. Show this calendar\n4. Show several calendars");
-		}
-
 	}
 
 	public static void main(String[] args) {
