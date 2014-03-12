ac097601a712e6199198226b541603cf5e40f21a
tonygj <tonygj@stud.ntnu.no>
2014-03-12 14:57:39 +0100

    Database med getAppointment()

--- ProjectPU/src/Classes/Database.java
+++ ProjectPU/src/Classes/Database.java
@@ -7,6 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class Database {
 
@@ -28,14 +29,14 @@ public class Database {
 		System.out.println("Update exectuted");
 	}
 
-//	public void removePersonFromDatabase(String statement, Connection conn) throws SQLException{
-//		//Create a query
-//		Statement stmt = (Statement) conn.createStatement();
-//		//Execute query
-//		stmt.executeUpdate(statement);
-//		DELETE FROM table_name WHERE some_column = some_value
-//
-//	}
+	//	public void removePersonFromDatabase(String statement, Connection conn) throws SQLException{
+	//		//Create a query
+	//		Statement stmt = (Statement) conn.createStatement();
+	//		//Execute query
+	//		stmt.executeUpdate(statement);
+	//		DELETE FROM table_name WHERE some_column = some_value
+	//
+	//	}
 
 	public Person getPersonFromDatabase(String mail, Connection conn){
 
@@ -63,7 +64,92 @@ public class Database {
 			return null;
 		}
 	}
-	
+
+	public Appointment getAppointment(int ID, Connection conn){
+
+		Appointment appointment;
+
+		try{
+
+			Statement stmt = (Statement) conn.createStatement();
+			stmt.executeQuery("SELECT * FROM larsfkl_felles.person where AppointmentID = '" + ID + "';");
+			ResultSet rs = stmt.getResultSet();
+
+			Calendar startTime = Calendar.getInstance();
+			Calendar finishTime = Calendar.getInstance();
+			Calendar SQLTime = Calendar.getInstance();
+
+			rs.next();
+
+			String stime = rs.getString(2);
+			SQLTime = convertSQLTimeToCalendarTime(stime);
+			startTime.set(startTime.HOUR_OF_DAY, SQLTime.get(Calendar.HOUR_OF_DAY));
+			startTime.set(startTime.MINUTE, SQLTime.get(Calendar.MINUTE));
+			startTime.set(startTime.SECOND, SQLTime.get(Calendar.SECOND));
+
+			String ftime = rs.getString(3);
+			SQLTime = convertSQLTimeToCalendarTime(ftime);
+			finishTime.set(finishTime.HOUR_OF_DAY, SQLTime.get(Calendar.HOUR_OF_DAY));
+			finishTime.set(finishTime.MINUTE, SQLTime.get(Calendar.MINUTE));
+			finishTime.set(finishTime.SECOND, SQLTime.get(Calendar.SECOND));
+
+			String date = rs.getString(4);
+			SQLTime = convertSQLDateToCalendarDate(date);
+			startTime.set(startTime.DATE, SQLTime.get(Calendar.DATE));
+			startTime.set(startTime.MONTH, SQLTime.get(Calendar.MONTH));
+			startTime.set(startTime.YEAR, SQLTime.get(Calendar.YEAR));
+			finishTime.set(finishTime.DATE, SQLTime.get(Calendar.DATE));
+			finishTime.set(finishTime.MONTH, SQLTime.get(Calendar.MONTH));
+			finishTime.set(finishTime.YEAR, SQLTime.get(Calendar.YEAR));
+
+			String descr = rs.getString(5);
+			String meetplace = rs.getString(6);
+			//		Person owner = rs.(10);
+			String email = rs.getString(10);
+			Person owner = getPersonFromDatabase(email, conn);
+
+			Alarm alarm = null;
+			appointment = new Appointment(startTime, finishTime, meetplace, descr, alarm, owner);
+			rs.close ();
+
+
+			return appointment;
+		}
+		catch(SQLException e){
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public Calendar convertSQLTimeToCalendarTime(String sTime){
+		Calendar time = Calendar.getInstance();
+
+		String[] sToTime = sTime.split(":");
+		Integer i = Integer.parseInt(sToTime[0]);
+		Integer j = Integer.parseInt(sToTime[1]);
+		Integer k = Integer.parseInt(sToTime[2]);
+		time.set(Calendar.HOUR_OF_DAY, i);
+		time.set(Calendar.MINUTE, j);
+		time.set(Calendar.SECOND, k);
+
+		return time;
+	}
+
+	public Calendar convertSQLDateToCalendarDate(String sDate){
+		Calendar date = Calendar.getInstance();
+
+		String[] sToDate = sDate.split("-");
+		Integer i = Integer.parseInt(sToDate[0]);
+		Integer j = Integer.parseInt(sToDate[1]);
+		Integer k = Integer.parseInt(sToDate[2]);
+		i = i - 1900;
+		date.set(Calendar.YEAR, i);
+		date.set(Calendar.MONTH, j);
+		date.set(Calendar.DATE, k);
+
+		return date;
+	}
+
 	public void removePerson(String mail, Connection conn){
 		try{
 			//Create a query
@@ -75,19 +161,9 @@ public class Database {
 		}
 	}
 
-//	public Appointment getAppointment(int ID, Connection conn){
-//		
-//		Appointment lars;
-//	
-//		Statement stmt = (Statement) conn.createStatement();
-//		stmt.executeQuery("");
-//		ResultSet rs = stmt.getResultSet();
-//		
-//		return null;
-//	}
-	
+
 	public void removeAppointment(int ID, Connection conn){
-		
+
 	}
 
 	public static void readDatabase(String res, Connection conn) throws SQLException{
