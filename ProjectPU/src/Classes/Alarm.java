61def49a038fcadc1c30d5cf4453aa7823f02f0a
larsfk <larsklingen@gmail.com>
2014-03-16 14:07:11 +0100

    Konstruktør snakker med database

--- ProjectPU/src/Classes/Alarm.java
+++ ProjectPU/src/Classes/Alarm.java
@@ -1,11 +1,15 @@
 package Classes;
 
+import java.sql.SQLException;
 import java.util.Calendar;
+
+import com.mysql.jdbc.Connection;
 
 public class Alarm {
 	
 	private String description;
 	private Calendar start;
+	Database db = new Database();
 	
 	public Alarm(int year, int month, int date, int hourOfDay, int minute, String description){
 		this.start = Calendar.getInstance();
@@ -16,11 +20,52 @@ public class Alarm {
 	
 	public Alarm(Appointment app, int start, String description){
 		this.start = Calendar.getInstance();
-		System.out.println("HEI");
 		this.start.add(app.getStarttime().MINUTE, -start);
 		this.description = description;
 	}
 
+	public Alarm(int year, int month, int date, int hourOfDay, int minute, String description, Connection conn){
+		this.start = Calendar.getInstance();
+		this.setStart(year, month, date, hourOfDay, minute);
+		this.description = description;
+		
+		try {
+			Integer Syear = this.add1900toCalendarYear(this.start.get(Calendar.YEAR));
+			Integer Smonth = this.start.get(Calendar.MONTH);
+			Integer Sday = this.start.get(Calendar.DATE);
+			Integer Shour = this.start.get(Calendar.HOUR_OF_DAY);
+			Integer Sminute = this.start.get(Calendar.MINUTE);
+
+			
+//			System.out.println("Saar " + Syear + " Sh " + Shour);
+//			System.out.println("Faar " + Fyear + " Fh " + Fhour);
+			
+			db.addToDatabase(   "insert into larsfkl_felles.alarm(alarm_id,time,date,type) " +
+					"values (" + db.generateAlarmID(conn) + ", '" 
+					+ Shour + ":" + Sminute + "','" 
+					+ Syear + "-" + Smonth + "-" + Sday + "','" 
+					+ getDescription() + "');", conn);
+		}
+		catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public Alarm(Appointment app, int start, String description, Connection conn){
+		this.start = Calendar.getInstance();
+		this.start.add(app.getStarttime().MINUTE, -start);
+		this.description = description;
+		
+		try {
+			db.addToDatabase( "insert into larsfkl_felles.alarm(time,type,alarm_id) values ('" + getStart() + "','" + getDescription() + "','" + db.generateAlarmID(conn) + "');", conn);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	public String getDescription() {
 		return description;
 	}
@@ -40,6 +85,10 @@ public class Alarm {
 		start.set(start.DAY_OF_MONTH, date);
 		start.set(start.HOUR_OF_DAY, hourOfDay);
 		start.set(start.MINUTE, minute);
+	}
+	
+	public static int add1900toCalendarYear(Integer year){
+		return year + 1900;
 	}
 	
 	@Override
