f72d0e88ccff7cb8adc29f60b715866808129290
oystein924 <oystein924>
2014-03-21 13:00:19 +0100

    dateCompare

--- ProjectPU/src/Classes/dateComparator.java
+++ ProjectPU/src/Classes/dateComparator.java
@@ -1,11 +1,18 @@
 package Classes;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Comparator;
 
 public class dateComparator implements Comparator<Appointment> {
 
 	public int compare(Appointment app0, Appointment app1) {
-		return app0.getDate().compareTo(app1.getDate());
+		try{
+			Database db = new Database();
+			Connection conn = db.getConnection();
+			return app0.getDate(conn).compareTo(app1.getDate(conn));
+		}
+		catch (SQLException e){e.printStackTrace(); return (Integer) null;}
 	}
 	
 }
